f4b4687a4e48b59786189adc859c2922b7e7535e
MES <bala.dhruv@gmail.com>
2018-07-05 13:47:11 -0700

    Dhruv is a God

--- server/Validation/profile.js
+++ server/Validation/profile.js
@@ -1,52 +1,27 @@
-const Validator = require('validator');
-const isEmpty = require('./is-empty');
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 const validateProfileInput = require('../../validation/profile');
 
-module.exports = function validateProfileInput(data) {
-    let errors = {};
+router.get('/test', (req, res) => res.json({msg: "Profile works!"}));
 
-data.handle = !isEmpty(data.handle) ? data.handle : '';
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const errors = {};
+    Profile.findOne({ us: req.user.id })
+        .then( profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
 
-if (!Validator.isLength(data.handle, {min: 2, max: 40 })) {
-    errors.handle = 'Handle needs to between 2 and 40 characters';
-}
-if (Validator.isEmpty(data.handle)) {
-    errors.handle = 'Profile handle is required';
-}
-if(!isEmpty(data.youtube)) {
-    if(!Validator.isURL(data.youtube)) {
-        errors.youtube = 'Not a valid URL';
-    }
-}
-if(!isEmpty(data.website)) {
-    if(!Validator.isURL(data.website)) {
-        errors.website = 'Not a valid URL';
-    }
-}
-if(!isEmpty(data.twitter)) {
-    if(!Validator.isURL(data.twitter)) {
-        errors.twitter = 'Not a valid URL';
-    }
-}
-if(!isEmpty(data.instagram)) {
-    if(!Validator.isURL(data.instagram)) {
-        errors.instagram = 'Not a valid URL';
-    }
-}
-if(!isEmpty(data.facebook)) {
-    if(!Validator.isURL(data.facebook)) {
-        errors.facebook = 'Not a valid URL';
-    }
-}
-if(!isEmpty(data.linkedin)) {
-    if(!Validator.isURL(data.linkedin)) {
-        errors.linkedin = 'Not a valid URL';
-    }
-}
-return { errors, isValid: isEmpty(errors)}
-};
-
-router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 router.get('/handle/:handle', (req, res) => {
     const errors = {};
     Profile.findOne({ handle: req.params.handle })
@@ -61,49 +36,82 @@ router.get('/handle/:handle', (req, res) => {
         .catch(err => res.status(404).json(err));
 });
 
-
-router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-router.get('/API/profile', (req, res) => {
+router.get('/user/:user_id', (req, res) => {
     const errors = {};
-    Profile.findOne({user: req.user.id})
+    Profile.findOne({ user: req.params.user_id })
         .populate('user', ['name', 'avatar'])
         .then(profile => {
-            if (!profile) {
+            if(!profile) {
                 errors.noprofile = 'There is no profile for this user';
-                return res.status(404).json(errors);
+                res.status(404).json(errors);
             }
             res.json(profile);
         })
         .catch(err => res.status(404).json(err));
 });
 
-    router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-        router.get('/api/profile/user/:user_id', (req, res) => {
-            const errors = {};
-            Profile.findOne({ user: req.params.user_id })
-                .populate('user', ['name', 'avatar'])
-                .then(profile => {
-                    if (!profile) {
-                        errors.noprofile = 'There is no profile for this user';
-                        return res.status(404).json(errors);
-                    }
-                    res.json(profile);
-                })
-                .catch(err => res.status(404).json(err));
-            })}
-    router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
-        router.get('/api/profile/all', (req, res) => {
-            const errors = {};
-            Profile.find()
-                .populate('user', ['name', 'avatar'])
-                .then(profile => {
-                    if (!profile) {
-                        errors.noprofile = 'There is no profile for this user';
-                        return res.status(404).json(errors);
-                    }
-                    res.json(profile);
-                })
-                .catch(err => res.status(404).json(err));
-        });}
+router.get('/all', (req, res) => {const errors = {};
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if(!profiles) {
+                errors.noprofile = 'There are no profiles';
+                return res.status(404).json();
+            }
+            res.json(profiles);
+        })
+        .catch(err => res.status(404).json({profile: 'There are no profiles'}));
+});
 
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+    if(!isValid) {
+        return res.status(400).json(errors);
+    }
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if(req.body.handle) profileFields.handle = req.body.handle;
+    if(req.body.company) profileFields.company = req.body.company;
+    if(req.body.website) profileFields.website = req.body.website;
+    if(req.body.location) profileFields.location = req.body.location;
+    if(req.body.bio) profileFields.bio = req.body.bio;
+    if(req.body.githubusername) profileFields.githubusername = req.body.githubusername;
+    profileFields.social ={};
+    if(req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    if(req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    if(req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    if(req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    if(req.body.instagram) profileFields.social.instagram = req.body.instagram;
 
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if(profile) {
+                Profile.findOneAndUpdate(
+                    { user: req.user.id },
+                    { $set: profileFields },
+                    {new: true}
+                ).then(profile => res.json(profile));
+            } else {
+                Profile.findOne({ handle: profileFields.handle })
+                    .then(profile => {
+                        if(profile) {
+                            errors.handle = 'That handle already exists';
+                            res.status(400).json(errors);
+                        }
+
+                        new Profile(profileFields).save()
+                            .then(profile => res.json(profile));
+                    })
+            }
+        });
+});
+router.delete('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+    Profile.findOneAndRemove({ user: req.user.id })
+        .then(() => {
+            User.findOneAndRemove({ user: req.user.id })
+                .then(() => res.json({ success: true }))
+                .catch(err => res.status(404).json(err));
+        }).catch(err => res.status(404).json(err));
+});
+
+module.exports = router;
